190734ad63507739a3affc5bdc4e2c1a00b08bdc
Milan Rawat <milanrawat086@gmail.com>
2021-06-18 16:04:29 +0530

    fixed fallback

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -37,7 +37,7 @@ export async function getStaticPaths() {
   client.close();
 
   return {
-    fallback: false,
+    fallback: "blocking",
     paths: meetups.map((meetup) => ({
       params: { meetupId: meetup._id.toString() },
     })),
